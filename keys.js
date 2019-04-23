aa9d6acb514c67cd0a953e7446dd9cf001359d5c
GunnyD <45931516+DeanJG@users.noreply.github.com>
2019-04-22 18:37:46 -0700

    added request for omdb

--- keys.js
+++ keys.js
@@ -4,3 +4,17 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+exports.omdb = {
+    id: process.env.OMDB_ID
+}
+
+exports.bands = {
+    id: process.env.BANDS_ID
+}
+
+// bandsintown request url
+// https://rest.bandsintown.com/artists/the%20killers?app_id=codingbootcamp
+
+// omdb request url
+// http://www.omdbapi.com/?apikey=trilogy&
--- liri.js
+++ liri.js
@@ -1,4 +1,22 @@
-// activates spotify API keys
+// activates .env file
 require("dotenv").config()
+// activates axios npm
+const axios = require(`axios`)
+// activates the keys stored in keys.js
 let keys = require("./keys.js")
-let spotify = new Spotify(keys.spotify)
+// activates the various api keys
+let spotify = keys.spotify
+let omdb = keys.omdb
+let bands = keys.bands
+// pulling command line input
+let [, , num1, num2] = process.argv
+
+
+// axios request for omdb
+axios.get(`http://www.omdbapi.com/?apikey=${omdb.id}&t=goodfellas`)
+  .then(r => {
+    console.log(r);
+  })
+  .catch(e => {
+    console.log(e);
+  });
