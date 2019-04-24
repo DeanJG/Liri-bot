9e234bcb39ddf432d6188abf181b7dab9b623dd8
GunnyD <45931516+DeanJG@users.noreply.github.com>
2019-04-23 19:27:56 -0700

    finished omdb and bands requests

--- liri.js
+++ liri.js
@@ -2,6 +2,8 @@
 require("dotenv").config()
 // activates axios npm
 const axios = require(`axios`)
+// activates moment npm
+const moment = require(`moment`)
 // activates the keys stored in keys.js
 let keys = require("./keys.js")
 // activates the various api keys
@@ -14,7 +16,17 @@ let [, , action, entry] = process.argv
 const reqFunc = (action, entry) => {
     switch (action) {
         case 'concert-this':
-
+            axios.get(`https://rest.bandsintown.com/artists/${entry}/events?app_id=${bands.id}`)
+                .then(({ data }) => {
+                    const {name, country, region, city} = data[0].venue
+                    console.log(`
+                    Venue: ${name}
+                    ----------------------------------------
+                    Location: ${city} ${region}, ${country}
+                    ----------------------------------------
+                    Show Date(Month/Day/Year): ${moment(data[0].datetime).format(`MM/DD/YYYY`)}
+                    `)
+                })
         break;
         case 'spotify-this-song':
             
@@ -23,15 +35,19 @@ const reqFunc = (action, entry) => {
             // axios request for omdb
             axios.get(`http://www.omdbapi.com/?apikey=${omdb.id}&t=${entry}`)
                 .then(({ data }) => {
+                    const {Title, Year, imdbRating, Ratings, Country, Language, Plot, Actors} = data
                     console.log(`
-                    Title: ${data.Title},
-                    Year: ${data.Year},
-                    IMDB Rating: ${data.imdbRating},
-                    Tomatoes Rating: ${data.Ratings[1].Value},
-                    Origin Country: ${data.Country},
-                    Language(s): ${data.Language},
-                    Plot: ${data.Plot},
-                    Actors: ${data.Actors}
+                    Title: ${Title}, ${Year}
+                    ----------------------------------------
+                    IMDB Rating:     ${imdbRating}
+                    Rotten Tomatoes: ${Ratings[1].Value}
+                    ----------------------------------------
+                    Origin Country: ${Country}
+                    Language(s): ${Language}
+                    ----------------------------------------
+                    Plot: ${Plot}
+                    ----------------------------------------
+                    Actors: ${Actors}
                     `)
                 })
                 .catch(e => {
